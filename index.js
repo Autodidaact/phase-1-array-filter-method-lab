4ba6d2b1b67fe55f81bd76cee334e504d6cb50ac
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-17 08:52:12 +0000

    Modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(array, check){
+    return array.filter((match) => {
+        return match.toUpperCase() === check.toUpperCase();
+    })
+}
+function fuzzyMatch(array, check){
+    
+   let returnArray = array.filter(function(el) {
+    return check[0] === el[0] && check[1] === el[1];
+   })
+   return returnArray;
+    
+
+}
+function matchName(arrayOffObjects, driverName){
+    return arrayOffObjects.filter(function(item){
+        return item.name === driverName;
+    })
+}
